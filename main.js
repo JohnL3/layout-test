fc7284135c97c3e6f5ce819b0de16a8450565bb1
John Long <longgithubmail@gmail.com>
2018-08-06 19:32:41 +0100

    'updates'

--- main.js
+++ main.js
@@ -1,18 +1,20 @@
-let offScreen = '-245px';
-
-$('#burger').click(function(){
-   if(offScreen === '-245px') {
-        $('aside').animate({left: '0'})
-        offScreen = '0';
-   } else {
-       $('aside').animate({left: '-245px'})
-       offScreen = '-245px';
-   }
-})
 
 
 $( window ).resize(function() {
   if($(window).width() > 657) {
 	  $('aside').css('left','0');
+  } else {
+	  $('aside').css('left','-245px');
   }
 });
+
+
+
+$('#burger').click(function(){
+
+   if($('aside').css('left') === '-245px') {
+        $('aside').animate({left: '0'})
+   } else {
+       $('aside').animate({left: '-245px'})
+   }
+})
